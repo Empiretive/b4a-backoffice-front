afacc14fd938ff9ee13c1dcbe7d78093da25ef12
Jesus Moreno <jesusmanuelmoreno.21@gmail.com>
2021-07-01 17:39:26 -0400

    image 2 base64 implementation

--- src/app/core/settings/company/company.component.ts
+++ src/app/core/settings/company/company.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseStorageService } from 'src/app/shared/services/firebase-storage.service';
+import { Image2base64Service } from 'src/app/shared/utils/image2base64.service';
 
 @Component({
   selector: 'b4a-company',
@@ -7,20 +8,26 @@ import { FirebaseStorageService } from 'src/app/shared/services/firebase-storage
   styleUrls: ['./company.component.css'],
 })
 export class CompanyComponent implements OnInit {
-  constructor(private storage: FirebaseStorageService) {}
+  constructor(
+    private storage: FirebaseStorageService,
+    private base64: Image2base64Service
+  ) {}
 
   ngOnInit(): void {}
   uploadFile(e: any) {
-    console.log(e);
     const file = e.target.files[0];
-    const ref = this.storage.refToFile('uploads/settings/logo.png');
-    const fileUpload = ref.put(file);
 
+    const image = this.base64.extraerBase64(file).then((img: any) => {
+      console.log(img);
+    });
+
+    // const ref = this.storage.refToFile('uploads/settings/logo.png');
+    // const fileUpload = ref.put(file);
     // fileUpload.percentageChanges().subscribe((res) => {
     //   console.log(res);
     // });
-    ref.getDownloadURL().subscribe((res) => {
-      console.log(res);
-    });
+    // ref.getDownloadURL().subscribe((res) => {
+    //   console.log(res);
+    // });
   }
 }
